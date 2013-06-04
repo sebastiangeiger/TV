f7cbc1b9c81ed351872c9495a331f124abb6b8c7
Sebastian Geiger <sebastian.geiger@gmail.com>
2013-06-04 21:50:58 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TV::Application.config.secret_token = 'b962afb0621916535320e4ac8d19c00805d9cd29c3ccf0696520be411525ed0ceee60cbe42e802e41907ab7c27a184e100004c510d9d94a9f7ba2e7158e69951'
