a2cc91f8741e2f6b0382a53fd593040fef1d4830
Sebastian Geiger <sebastian.geiger@gmail.com>
2013-07-17 20:29:45 +0200

    grunt-connect-proxy is working
      - It is tunneling localhost:9000/api/* requests to the matching route
        on localhost:3000/api/*
      - Adding an /api/echo route to test the grunt proxy

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -2,6 +2,7 @@
 'use strict';
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
 };
@@ -59,11 +60,21 @@ module.exports = function (grunt) {
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost'
       },
+      proxies: [
+        {
+          context: '/api',
+          host: 'localhost',
+          port: 3000,
+          https: false,
+          changeOrigin: false
+        }
+      ],
       livereload: {
         options: {
           middleware: function (connect) {
             return [
               lrSnippet,
+              proxySnippet,
               mountFolder(connect, '.tmp'),
               mountFolder(connect, yeomanConfig.app)
             ];
@@ -312,6 +323,7 @@ module.exports = function (grunt) {
 
     grunt.task.run([
       'clean:server',
+      'configureProxies',
       'concurrent:server',
       'connect:livereload',
       'open',
