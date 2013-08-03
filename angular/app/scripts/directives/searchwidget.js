d8102f5036e2d1979c843fb90b2b7999ae41ce7d
Sebastian Geiger <sebastian.geiger@gmail.com>
2013-08-03 12:07:31 +0200

    Pulling html and css for searchwidget out

--- angular/app/scripts/directives/searchwidget.js
+++ angular/app/scripts/directives/searchwidget.js
@@ -3,7 +3,7 @@
 angular.module('tvApp')
   .directive('searchwidget', function () {
     return {
-      template: "<div ng-controller='SearchCtrl'> <input type='text' id='search-query' autofocus='autofocus' ng-model='searchTerm' ng-change='updateSearchResults(searchTerm)' /> <searchresults results='searchResults'></searchresults> </div> ",
+      templateUrl: "views/directives/searchwidget.html",
       restrict: 'E'
     };
   });
